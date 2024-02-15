30ee7836cd214c532a751b8f64babc20a67582c0
Therakar <matteo.pau200@gmail.com>
2024-02-15 13:40:49 +0100

    Implementing nested routes and index routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,19 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        {/* Nested routes */}
+        <Route path="app" element={<AppLayout />}>
+          {/* In Nested Routes I don't necessary need a component to 
+          pass in the element, I can pass a normal JSX element also */}
+          {/*This is an index route*/}
+          <Route index element={<p>List of cities</p>} />{" "}
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="login" element={<Login />} />
         {/* This route with path '*' will get all the routes that 
         are different from the one I defined */}
