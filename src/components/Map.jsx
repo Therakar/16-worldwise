06736400c9b6971d22f71e3069f5185d4ae88d84
Therakar <matteo.pau200@gmail.com>
2024-02-17 16:55:22 +0100

    Programmatic Navigation with useNavigate custom hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,20 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
+  const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
 
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
-  if (!lat || !lng)
-    return (
-      <div className={styles.mapContainer}>
-        <h1>MAP</h1>
-      </div>
-    );
-
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
       <h1>MAP</h1>
       <h2>
         Position: {lat}, {lng}
