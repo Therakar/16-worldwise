2b64aae1c39f9efc5a01e03a75ebcb68506d7313
Therakar <matteo.pau200@gmail.com>
2024-02-21 13:04:24 +0100

    adds markers with custom text for every city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,15 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
+import { useCities } from "../contexts/CitiesContext";
+
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useState } from "react";
 
 function Map() {
   const navigate = useNavigate();
+  const { cities, getFlag } = useCities();
+
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
@@ -25,11 +29,16 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{getFlag(city.emoji)}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
